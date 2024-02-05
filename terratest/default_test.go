1b1f30fb2d141cb6c0dd2053109fc84151605f37
CI Pipeline <ci_email@localhost>
2024-02-06 00:30:15 +0100

    fix: review verify step of terratest

--- terratest/default_test.go
+++ terratest/default_test.go
@@ -28,6 +28,14 @@ func TestEndToEndDeploymentScenario(t *testing.T) {
         terraform.InitAndApply(t, terraformOptions)
     })
 
+    test_structure.RunTestStage(t, "idempotence", func() {
+
+        terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+
+        // Triggers to check Terraform configuration is idempotent when a second apply results in 0 changes
+        terraform.ApplyAndIdempotent(t, terraformOptions)
+    })
+
     test_structure.RunTestStage(t, "verify", func() {
 
         terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
@@ -52,7 +60,7 @@ func testEndpointExists(t *testing.T, terraformOptions *terraform.Options) {
     // Run `terraform output` to get the value of an output variable
     username := terraform.Output(t, terraformOptions, "elasticsearch_username")
     password := terraform.Output(t, terraformOptions, "elasticsearch_password")
-    elasticsearch_endpoint := strings.Replace(strings.Join(terraform.OutputList(t, terraformOptions, "elasticsearch_https_endpoint"), ""), "https://", "", 1)
+    elasticsearch_endpoint := strings.Replace(terraform.Output(t, terraformOptions, "elasticsearch_https_endpoint"), "https://", "", 1)
     elasticsearch_endpoint = strings.Replace(elasticsearch_endpoint, ":9243", "", 1)
 
     _, err := http.Get(fmt.Sprintf("https://%s:%s@%s", username, password, elasticsearch_endpoint))
